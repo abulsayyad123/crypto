578fe736689dd7fde0ea76f573c4e0a37506e897
AbulAsar S <abul.asar22@gmail.com>
2022-02-20 18:39:10 +0530

    Add tailwind and styling for table

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :websockex_demo, WebsockexDemoWeb.Endpoint,
   secret_key_base: "mPJzRD7bnfcN1Ll5pB588nilNjufLJ14dzroRmVkkB1rm8Cz1lJlbtuxjIEtVoBY",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
--- lib/websockex_demo_web/live/sfox_data_live.ex
+++ lib/websockex_demo_web/live/sfox_data_live.ex
@@ -10,10 +10,69 @@ defmodule WebsockexDemoWeb.SfoxDataLive do
 
   def render(assigns) do
     ~L'''
-      <h1>Sfox Live</h1>
-      <%= for coin <- @coins do %>
-        <%= coin["payload"]["amount"] %>
-      <% end %>
+      <h1 class="text-red-500 text-5xl font-bold text-center">Sfox Live</h1>
+      <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+              <table class="min-w-full">
+                <thead class="bg-white border-b">
+                  <tr>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      #
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Exchange
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      High
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Last
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Low
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Volume
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Amount
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <%= @coins |> Enum.with_index |> Enum.map(fn({coin, index}) ->  %>
+                    <tr class="<%= if rem(index, 2) == 0 do "bg-gray-100" else "bg-white" end %> border-b">
+                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      <%= index+1 %>
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <%= coin["payload"]["exchange"] %>
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <%= coin["payload"]["high"] %>
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <%= coin["payload"]["last"] %>
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <%= coin["payload"]["low"] %>
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <%= coin["payload"]["volume"] %>
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <%= coin["payload"]["amount"] %>
+                      </td>
+                    </tr>
+                  <% end) %>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     '''
   end
 
